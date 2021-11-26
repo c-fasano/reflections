2bafc88961b89f6f99e4d2db0cfced11cdd5271c
Cole-Fasano <cole.fasano@outlook.com>
2021-11-26 15:20:48 -0600

    add logout url - fix non-logged in nav

--- reflections/settings.py
+++ reflections/settings.py
@@ -124,7 +124,7 @@ LOGIN_URL = ''
 
 LOGIN_REDIRECT_URL = ''
 
-LOGOUT_REDIRECT_URL = ''
+LOGOUT_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
