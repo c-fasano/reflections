2c2d86c763f4efd7e89a553f990fdeebf71eaee0
Cole-Fasano <cole.fasano@outlook.com>
2021-11-27 19:14:19 -0600

    Change default value for date field to allow past date

--- main_app/models.py
+++ main_app/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import datetime
 
 # Create your models here.
 
 class Memory(models.Model):
   name = models.CharField(max_length=100)
   location = models.CharField(max_length=100)
-  date = models.DateField('date of memory')
+  date = models.DateField(default=datetime.now)
   details = models.TextField(max_length=1000, default='')
   user = models.ForeignKey(User, on_delete=models.CASCADE)
 
